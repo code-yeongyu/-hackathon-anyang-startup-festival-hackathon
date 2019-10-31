30ee7ba1c41927eb7f369e8786426a3aadc80193
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-11-01 00:21:12 +0900

    Add login token feature

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from app import views
+from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
+    url(r'^signin/$', drf_views.obtain_auth_token, name='auth'),
     url(r'^signup/$', views.sign_up),  # 프로필 정보를 얻거나 , 변경하는 라우트
     url(r'^video/(?P<pk>[0-9]+)/$',
         views.VideoDetail.as_view()),  # 해당되는 게시글을 얻는 라우트
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'corsheaders',
+    'django.contrib.messages', 'rest_framework',
+    'rest_framework.authtoken', 'django.contrib.staticfiles', 'corsheaders',
     'drf_yasg', 'app'
 ]
 
